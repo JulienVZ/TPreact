e7bf918772269e6e238afc4a30e0946361577044
julienvouilloz <julienvouilloz2000@gmail.com>
2026-05-04 15:11:06 +0200

    implémentation du parent App.jsx et App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+import {Card} from './components/Card';
+import {Button} from './components/Button';
+import { useState } from 'react';
+import TaskInput from './components/TaskInput';
 
 function App() {
+    const [tasks,
+        setTasks] = useState ([]) ;
+    const addTask = (text) => {
+        // Logique pour ajouter une tache ici
+    } ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+            <Card />
+            <Button />
+        <Footer />
+    </div>,
+
+    <div className= "container">
+        <h1>Ma Todo List </h1>
+        <TaskInput onAddTask={addTask} /> <ul>{/* La liste des taches viendra ici */}</ul>
     </div>
   );
 }
